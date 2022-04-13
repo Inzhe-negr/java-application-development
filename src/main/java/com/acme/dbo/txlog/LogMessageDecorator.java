f150c9eb8bea26c3ea82f8a85243770be32106a1
Mikhail Morozov <mmorozov@yandex.ru>
2022-04-13 23:36:35 +0300

    Mikhail Morozov. Exercise 01 v0.2.

--- src/main/java/com/acme/dbo/txlog/LogMessageDecorator.java
+++ src/main/java/com/acme/dbo/txlog/LogMessageDecorator.java
@@ -4,12 +4,13 @@ import java.lang.reflect.Type;
 import java.util.HashMap;
 
 public class LogMessageDecorator {
+    private static final String PRIMITIVE_SUFFIX = "primitive: ";
     private static final HashMap<Type, String> PREFIX_MAP = new HashMap<>();
 
     static {
-        PREFIX_MAP.put(Boolean.class, "primitive: ");
-        PREFIX_MAP.put(Byte.class, "primitive: ");
-        PREFIX_MAP.put(Integer.class, "primitive: ");
+        PREFIX_MAP.put(Boolean.class, PRIMITIVE_SUFFIX);
+        PREFIX_MAP.put(Byte.class, PRIMITIVE_SUFFIX);
+        PREFIX_MAP.put(Integer.class, PRIMITIVE_SUFFIX);
         PREFIX_MAP.put(Character.class, "char: ");
         PREFIX_MAP.put(String.class, "string: ");
         PREFIX_MAP.put(Object.class, "reference: ");
